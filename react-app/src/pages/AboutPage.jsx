985ae8a23c67bc8422a91107a914e6e646620fb8
Zygimantasdom <zygimantasdomeikis@yahoo.com>
2024-12-03 20:14:12 +0200

    homePage, aboutPage style added

--- react-app/src/pages/AboutPage.jsx
+++ react-app/src/pages/AboutPage.jsx
@@ -1,7 +1,19 @@
 import React from "react";
 
 const AboutPage = () => {
-  return <div>About us</div>;
+  return (
+    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
+      <h1>Apie mus</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer cursus
+        interdum tortor id imperdiet. Nulla feugiat nunc arcu, vitae ullamcorper
+        sem sodales a. Phasellus tincidunt et ante tristique mollis. Vivamus
+        diam mauris, tincidunt sed cursus ut, maximus ac enim. Aliquam lobortis
+        diam at sapien feugiat lobortis. Sed ut efficitur felis. Integer
+        sagittis mattis sodales.
+      </p>
+    </div>
+  );
 };
 
 export default AboutPage;
